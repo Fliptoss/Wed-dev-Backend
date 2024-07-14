8239237215b7edba7e5164083121e6c4359e2a5a
Fliptoss <samirman.0009@gmail.com>
2024-07-15 03:59:26 +0600

    first commit

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,15 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+
+import superheroes from 'superheroes';
+
+// Log the superheroes object to check available functions
+const name = superheroes.random();
+
+console.log(`I am ${name}!`);
+
